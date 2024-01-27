64e2b1d88b03bd2b22c609a67b2ddee7725eec6d
jorge871 <jg303114@gmail.com>
2024-01-27 17:27:55 -0300

    fix the code

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,9 +2,10 @@
 def fizzbuzz():
     for fzbz in range(1, 101):
         if fzbz % 3 == 0:
-            print("Fizz", end=' ')
+            print("Fizz", end='')
         if fzbz % 5 == 0:
-            print("Buzz", end=' ')
+            print("Buzz", end='')
         if fzbz % 3 != 0 and fzbz % 5 != 0:
-            print(fzbz, end=' ')
-    print("", end='')
+            print(fzbz, end='')
+        print("", end=' ')
+
