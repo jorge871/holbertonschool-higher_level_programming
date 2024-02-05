0a91c684ee92c2fd15b57bfde2a8707d9b01a202
jorge871 <jg303114@gmail.com>
2024-02-04 21:30:31 -0300

    add task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square():
+    """Square class"""
+    __size = 0
+
+    def __init__(self, size):
+        """init with size of square"""
+        self.__size = size
