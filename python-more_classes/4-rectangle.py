8b600a131e104f09a65eaa5cb8a41d228086427b
jorge871 <jg303114@gmail.com>
2024-02-09 18:37:36 +0000

    added task 2-5

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Rectangle"""
+
+
+class Rectangle:
+    """Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height != 0 and self.__width != 0:
+            return (self.__width * 2) + (self.__height * 2)
+        else:
+            return 0
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ""
+
+        return ((("#" * self.__width) + "\n") * (self.__height - 1))\
+            + ("#" * self.__width)
+
+    def __repr__(self):
+        return "Rectangle(" + str(self.__width) + ", "\
+            + str(self.__height) + ")"
